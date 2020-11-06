8b8538b5e3aee736b025494f631bf803c088268a
ronkroeger141 <74055293+ronkroeger141@users.noreply.github.com>
2020-11-06 18:52:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html> 
 <html> 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-YQ7V4MYRQ1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-YQ7V4MYRQ1');
+</script>
 <head> 
 	<title>Clean & Delicious</title> 
 	<style type="text/css"> 
